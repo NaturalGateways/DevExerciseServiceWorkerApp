cc958215d5087f00ec31280a38a504d724b3dd35
Steve Moore <smoore@sfi.com.au>
2021-06-27 20:50:20 +1000

    Aborted implementation of Serilog for logging

---
+++ ServiceWorkerMobileApp/NG.ServiceWorker.CoreServices.Shared/DefaultImplementations/Serilog/SerilogLogService.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Serilog;
+
+namespace NG.ServiceWorker.CoreServices.Serilog
+{
+    public class SerilogLogService : ILogService
+    {
+        #region Base
+
+        /// <summary>The cached created loggers.</summary>
+        private Dictionary<string, ILogger> m_loggersByName = new Dictionary<string, ILogger>();
+
+        /// <summary>Constructor.</summary>
+        public SerilogLogService()
+        {
+            // Initialise
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.Console()
+                .CreateLogger();
+        }
+
+        #endregion
+
+        #region ILogService implementation
+
+        /// <summary>Creates a logger.</summary>
+        public ILogger CreateLogger(string loggerName)
+        {
+            lock (m_loggersByName)
+            {
+                if (m_loggersByName.ContainsKey(loggerName))
+                {
+                    return m_loggersByName[loggerName];
+                }
+                ILogger newLogger = new SerilogLogger(loggerName);
+                m_loggersByName.Add(loggerName, newLogger);
+                return newLogger;
+            }
+        }
+
+        /// <summary>Getter for the currently logged sessions.</summary>
+        public IEnumerable<LogsModel.LogsSession> GetSessions()
+        {
+            return Array.Empty<LogsModel.LogsSession>();
+        }
+
+        /// <summary>Getter for the log entries for a session.</summary>
+        public IEnumerable<LogsModel.LogsEntry> GetEntriesForSessionId(string sessionId)
+        {
+            return Array.Empty<LogsModel.LogsEntry>();
+        }
+
+        /// <summary>Getter for the entry detail.</summary>
+        public LogsModel.LogEntryDetailJson GetEntryDetailForEntryId(int entryId)
+        {
+            return null;
+        }
+
+        #endregion
+    }
+}
---
+++ ServiceWorkerMobileApp/NG.ServiceWorker.CoreServices.Shared/DefaultImplementations/Serilog/SerilogLogger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+using Serilog;
+
+namespace NG.ServiceWorker.CoreServices.Serilog
+{
+    public class SerilogLogger : ILogger
+    {
+        #region Base
+
+        /// <summary>The name of the logger.</summary>
+        private string m_loggerName = null;
+
+        /// <summary>Constructor.</summary>
+        public SerilogLogger(string loggerName)
+        {
+            m_loggerName = loggerName;
+        }
+
+        #endregion
+
+        #region ILogger implementation
+
+        public void Trace(string message) { Log.Verbose($"{m_loggerName}: {message}"); }
+        public void Info(string message) { Log.Information($"{m_loggerName}: {message}"); }
+        public void Warn(string message) { Log.Warning($"{m_loggerName}: {message}"); }
+        public void Error(string message) { Log.Error($"{m_loggerName}: {message}"); }
+        public void Fatal(string message) { Log.Fatal($"{m_loggerName}: {message}"); }
+        public void Warn(string message, Exception ex) { Log.Warning(ex, $"{m_loggerName}: {message}"); }
+        public void Error(string message, Exception ex) { Log.Error(ex, $"{m_loggerName}: {message}"); }
+        public void Fatal(string message, Exception ex) { Log.Fatal(ex, $"{m_loggerName}: {message}"); }
+
+        #endregion
+    }
+}
