d9baa440d3ffffbc4122e8f26ccfa7f38ca7ef82
Steve <tri.steve.area@gmail.com>
2021-09-23 01:08:47 +1000

    Offline API: Fixed function error

--- ServiceWorkerApi/ServiceWorker.Api.Lambda/Function.cs
+++ ServiceWorkerApi/ServiceWorker.Api.Lambda/Function.cs
@@ -12,21 +12,37 @@ namespace ServiceWorker.Api.Lambda
 {
     public class Function
     {
-        public class AnonGetRequestDto
-        {
-            public ApiRequestAnonDto body { get; set; }
-        }
-
-        public async Task<ApiResponseDto> AnonEndpointHandler(AnonGetRequestDto input, ILambdaContext context)
+        public async Task<ApiResponseDto> AnonEndpointHandler(Dto.AnonGetRequestDto input, ILambdaContext context)
         {
             // Create service
-            AnonGetService getService = new AnonGetService
+            AnonGetService getService = new AnonGetService(new Helpers.CloudFormationTableNameProvider())
             {
                 AwsService = new Natural.Aws.LambdaAwsService()
             };
 
             // Execute request
-            return await getService.ExecuteRequest(input.body);
+            return await getService.ExecuteRequest(ConvertRequestBody(input.body));
+        }
+
+        private ApiRequestAnonDto ConvertRequestBody(Dto.AnonGetRequestBodyDto rawBody)
+        {
+            // Parse request type
+            if (string.IsNullOrEmpty(rawBody.RequestType))
+            {
+                throw new ApiException("A request type must be provided.");
+            }
+            ApiRequestAnonType convertedType = ApiRequestAnonType.GetInfo;
+            if (Enum.TryParse<ApiRequestAnonType>(rawBody.RequestType, true, out convertedType) == false)
+            {
+                throw new ApiException($"Request type '{rawBody.RequestType}' is unrecognised.");
+            }
+            // Create and return converted version
+            ApiRequestAnonDto convertedBody = new ApiRequestAnonDto
+            {
+                RequestType = convertedType,
+                GetRefData = rawBody.GetRefData
+            };
+            return convertedBody;
         }
     }
 }
